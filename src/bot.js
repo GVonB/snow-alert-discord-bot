b7586a4490195ecab5bd554b302198349fb6856b
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-02-03 22:24:06 -0500

    Boilerplate bot code

--- src/bot.js
+++ src/bot.js
@@ -5,3 +5,12 @@ const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 // Require discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
+// Create a client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with client's token
+client.login(DISCORD_TOKEN);
