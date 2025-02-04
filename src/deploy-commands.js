38c9eb647ceff8a05ff989d890736659e251473b
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-02-03 23:39:08 -0500

    Add standard node modules for file system and path handling in deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -6,3 +6,8 @@ const { DISCORD_CLIENT_ID, DISCORD_GUILD_ID, DISCORD_TOKEN } = process.env;
 
 // Require necessary discord.js features
 const { REST, Routes } = require('discord.js');
+
+// Standard node modules
+const fs = require('node:fs');
+const path = require('node:path');
+
