145d4acfae405645a6833c694dc4054a7a101d7e
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-02-03 22:21:22 -0500

    Require dotenv for token management and import discord.js classes

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,7 @@
-require('dotenv').config()
+// Require .env for tokens
+require('dotenv').config();
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+// Require discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 
