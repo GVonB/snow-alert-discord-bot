927627825ea902235aa93e999201d81c0049a48b
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-02-05 11:46:43 -0500

    Indentation linting

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Provides help information for commands'),
-    async execute(interaction) {
-        await interaction.reply('Here are the available commands...');
-    },
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Provides help information for commands'),
+	async execute(interaction) {
+		await interaction.reply('Here are the available commands...');
+	},
 };
