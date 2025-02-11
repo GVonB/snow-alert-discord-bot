01a1791f5c80cc1c8ec1699b6badf29a9a15bdb3
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-02-11 10:56:48 -0500

    Sample implementation in snow.js

--- src/commands/snow.js
+++ src/commands/snow.js
@@ -1,10 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { fetchWeather } =  require('../utils/weather');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('snow')
 		.setDescription('Checks if it will snow this week'),
 	async execute(interaction) {
-		await interaction.reply('Checking weather for snow...');
+        const forecast = await fetchWeather();
+
+		await interaction.reply(`Forecast: ${forecast}`);
 	},
 };
